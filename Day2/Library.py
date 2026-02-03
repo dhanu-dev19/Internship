a94ad58f60692e9a5bd430c06c19ce79859f97cc
Dhanush D <dhanushddhanushd668@gmail.com>
2026-02-03 18:24:01 +0530

    Day 2

---
+++ Day2/Library.py
@@ -0,0 +1,32 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def display_book_details(self):
+        print("Title :", self.title)
+        print("Author:", self.author)
+
+
+class IssuedBook(Book):
+    def __init__(self, title, author, issued_to, issued_date):
+        super().__init__(title, author)
+        self.issued_to = issued_to
+        self.issued_date = issued_date
+
+    def display_issued_book_details(self):
+        self.display_book_details()
+        print("Issued To  :", self.issued_to)
+        print("Issued Date:", self.issued_date)
+
+
+
+issued_book = IssuedBook(
+    "Clean Code",
+    "Robert C. Martin",
+    "Dhanush",
+    "03-02-2026"
+)
+
+
+issued_book.display_issued_book_details()
